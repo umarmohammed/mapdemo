0c81f16a48a3cd574b09eaba3e249140a6a57011
Umar Mohammed <umar@teacherapp.co.uk>
2019-05-18 19:17:53 +0100

    move material to seperate module
    #23

---
+++ src/material/material.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+
+const materialModules = [
+  MatButtonModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatDialogModule,
+  MatSelectModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatSliderModule,
+  MatInputModule
+];
+
+@NgModule({
+  imports: [...materialModules],
+  exports: [...materialModules]
+})
+export class MaterialModule {}
