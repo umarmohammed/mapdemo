991d5a6b30746b80c1b9e6d2b6419e9d1e208538
umarmohammed <umar@thinkincode.co.uk>
2019-05-19 16:01:18 +0100

    Update firebase.config.ts

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,11 +1,8 @@
-export const environment = {
-  production: false,
-  firebase: {
+export const firebase = {
     apiKey: '<your-key>',
     authDomain: '<your-project-authdomain>',
     databaseURL: '<your-database-URL>',
     projectId: '<your-project-id>',
     storageBucket: '<your-storage-bucket>',
     messagingSenderId: '<your-messaging-sender-id>'
-  }
 };
