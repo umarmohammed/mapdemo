b999c885b8208ae648f535f7af9857667c5c5e8f
Umar Mohammed <umar@teacherapp.co.uk>
2019-06-16 18:07:01 +0100

    save and load styles
    #52

--- src/app/containers/config-editor/config-editor.component.ts
+++ src/app/containers/config-editor/config-editor.component.ts
@@ -1,4 +1,9 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+
+import * as fromStore from '../../store';
+import { FileSaverService } from 'ngx-filesaver';
+import { HereTileLayer } from 'src/app/models/here-tile-layer.model';
 
 @Component({
   selector: 'app-config-editor',
@@ -7,4 +12,33 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class ConfigEditorComponent {
   @Output() toggleNav = new EventEmitter();
+
+  private layers: HereTileLayer[];
+
+  constructor(
+    private store: Store<fromStore.State>,
+    private _FileSaverService: FileSaverService
+  ) {
+    this.store
+      .pipe(select(fromStore.getLayers))
+      .subscribe(layers => (this.layers = layers));
+  }
+
+  loadMapStyle(value: string) {
+    const layers: HereTileLayer[] = JSON.parse(value);
+    this.store.dispatch(fromStore.loadLayers({ layers }));
+  }
+
+  onFileSelected(fileInput: HTMLInputElement) {
+    if (fileInput.files && fileInput.files.length) {
+      const fileReader = new FileReader();
+      fileReader.readAsText(fileInput.files.item(0));
+      fileReader.onload = () => this.loadMapStyle(fileReader.result as string);
+      fileInput.value = '';
+    }
+  }
+
+  saveStyles() {
+    this._FileSaverService.saveText(JSON.stringify(this.layers), 'styles.json');
+  }
 }
